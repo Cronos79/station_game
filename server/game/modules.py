20620fb5ac920c845c1318c1e681e62b6d843067
Matt B <cronos1979@live.com>
2026-01-29 20:46:37 -0600

    v1: remove prototype station/world/modules (universe-first rewrite)

--- server/game/modules.py
+++
@@ -1,9 +0,0 @@
-MODULES = {
-    "solar_array": {
-        "name": "Solar Array",
-        "cost": {"credits": 50},
-        "rates": {"power": 1.5},
-        "build_time": 10.0,  # seconds
-        "max_per_station": 3,
-    },
-}
